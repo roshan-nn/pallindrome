c3154570ce815568963508d5faca89df029540bb
roshan-nn <roshan.thomas767@gmail.com>
2025-11-20 10:57:06 +0530

    fibo

---
+++ fibo.js
@@ -0,0 +1,17 @@
+class Solution {
+    nthFibonacci(n) {
+       if(n===0)
+       {
+           return 0;
+       }
+       let first=0;
+       let second=1;
+       for(let i=2;i<=n;i++)
+       {
+           let next=first + second;
+           first=second;
+           second= next;
+       }
+       return second;
+    }
+}
